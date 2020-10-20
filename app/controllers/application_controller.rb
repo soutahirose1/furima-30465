8a9b15bb3e79f15ba45fdb19a5d111b80138f9aa
soutahirose1 <71875688+soutahirose1@users.noreply.github.com>
2020-10-21 05:09:33 +0900

    rubocop導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'hirose' && password == '0929'
+    end
+  end
 end
